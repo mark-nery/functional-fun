8f93e53a0a1f9510ecde96c80b236597a85f2fc0
mark nery <marknery@gmail.com>
2013-07-13 23:48:45 -0600

    init

---
+++ functional_javascript.js
@@ -0,0 +1,24 @@
+#functional look into solving http://projecteuler.net/problem=1
+
+function isMultipleOf(n) {
+  if( _.isArray(n))
+    return function(x) {
+      return _.chain(n)
+        .map(function (n) { return x % n == 0 } )
+        .contains(true)
+        .value()
+    }
+  else
+    return function(x) { return x % n == 0 } 
+}    
+
+function multiplesList(upto,filterFunction) {
+  return _.filter(_.range(upto), filterFunction))              
+}
+
+function listCruncher(upto, multiples, operation){
+  return _.chain(multiplesList(upto,isMultipleOf(multiples)))
+    .reduce(operation)
+    .value()
+}
+
